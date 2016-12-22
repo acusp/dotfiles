b0b2e15a43ec756523d31c1000b7d97a91c193ac
acusp <acusp.xu@gmail.cn>
2016-12-22 22:51:19 +0800

    Update | add backup and link func

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+backup=$HOME/backup/dotfiles
+
 # backup & link dot files
+baclin() {
+    echo "1. Backup and Link..."
+    mkdir -p $HOME/backup/dotfiles
+    mv $HOME/.vimrc $backup
+    mv $HOME/.zshrc $backup
+    mv $HOME/.gitconfig $backup
+    ln -sf $(pwd)/vimrc $HOME/.vimrc
+    ln -sf $(pwd)/zshrc $HOME/.zshrc
+    ln -sf $(pwd)/gitconfig $HOME/.gitconfig
+}
 
 # install tools
+
+# main
+baclin
