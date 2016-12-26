e0bf59512eb060aff81bba5a72d501c993094074
acusp <acusp.xu@gmail.com>
2016-12-26 21:09:06 +0800

    Update | add some details

--- install.sh
+++ install.sh
@@ -18,59 +18,73 @@ baclin() {
     do_backup .gitconfig
     ln -sf $(pwd)/files/vimrc $HOME/.vimrc
     ln -sf $(pwd)/files/gitconfig $HOME/.gitconfig
-    if [ $os == 'linux' ];then
+    if [ $os == 'Linux' ];then
         ln -sf $(pwd)/files/zshrc_linux $HOME/.zshrc
     elif [ $os == 'Darwin' ];then
         ln -sf $(pwd)/files/zshrc_mac $HOME/.zshrc
     fi
 }
 
-# install tools
-install() {
-    if [ $os == 'linux' ];then
-        echo "2. install requirements..."
-        sudo apt-get install -y git wget curl build-essential cmake python-dev python3-dev
-        sudo apt-get install -y proxychains
+requirement() {
+    if [ $os == 'Linux' ];then
+        sudo apt-get remove -y vim-common
+        sudo add-apt-repository -y ppa:jonathonf/vim   # vim version 8
         sudo add-apt-repository -y ppa:hzwhuang/ss-qt5
-        sudo apt-get update
-        sudo apt-get install -y shadowsocks-qt5
+        sudo apt update
+        sudo apt-get install -y vim vim-gnome git wget curl build-essential cmake python-dev python3-dev ppa-purge proxychains shadowsocks-qt5
+    elif [ $os == 'Darwin' ];then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        brew install git wget proxychains-ng cmake
+        wget -c https://github.com/shadowsocks/shadowsocks-iOS/releases/download/2.6.3/ShadowsocksX-2.6.3.dmg
+    fi
+}
 
-        echo "3. install some tools about vim..."
-        # Vundle
-        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim() {
+    # Vundle
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+    # Powerlin fonts
+    git clone https://github.com/powerline/fonts.git $HOME/.vim/fonts
+    cd $HOME/.vim/fonts
+    ./install.sh
+
+    if [ $os == 'Linux' ];then
         # ctags
         sudo apt-get install -y ctags
         # cscope
         sudo apt-get install -y cscope
 
-        echo "4. install some tools about zsh..."
-        # zsh
-        sudo apt-get install -y zsh
-        # oh-my-zsh
-        git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-        # autojump
-        sudo apt-get install -y autojump
-
     elif [ $os == 'Darwin' ];then
-        echo "2. install requirements..."
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        brew install git wget proxychains-ng cmake
-        wget -c https://github.com/shadowsocks/shadowsocks-iOS/releases/download/2.6.3/ShadowsocksX-2.6.3.dmg
-
-        echo "3. install some tools about vim..."
-        # Vundle
-        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
         # ctags
         sudo brew install ctags
         # cscope
         sudo brew install cscope
+    fi
+}
 
-        echo "4. install some tools about zsh..."
-        # oh-my-zsh
-        git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+zsh() {
+    # oh-my-zsh
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+    if [ $os == 'Linux' ];then
+        # zsh
+        sudo apt-get install -y zsh
+        # autojump
+        sudo apt-get install -y autojump
+    elif [ $os == 'Darwin' ];then
         # autojump
         brew install autojump
     fi
+}
+
+install() {
+    echo "2. install some requirements..."
+    requirement
+
+    echo "3. install some tools about vim..."
+    vim
+
+    echo "4. install some tools about zsh..."
+    zsh
 }
 
 # main()
